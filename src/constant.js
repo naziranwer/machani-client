fe29f2106b584ee6d77640f239de18c1425a46ec
Nazir <anwernazir674@gmail.com>
2024-05-10 14:23:04 +0530

    new host

--- src/constant.js
+++ src/constant.js
@@ -1,3 +1,3 @@
-export const host="http://localhost:4000"
+//export const host="http://localhost:4000"
 export const bg_url='https://static.vecteezy.com/system/resources/thumbnails/007/164/537/small/fingerprint-identity-sensor-data-protection-system-podium-hologram-blue-light-and-concept-free-vector.jpg'
-
+ export const host="https://machani-server.onrender.com/"
